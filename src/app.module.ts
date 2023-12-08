fe12b40aed127396ad1ff7a9791d2522c0e59360
yurii2007 <yurash862@gmail.com>
2023-12-08 21:45:47 +0200

    prisma global module body type pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookMark } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookMark],
+  imports: [AuthModule, UserModule, BookMark, PrismaModule],
   controllers: [],
   providers: [],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,18 @@
+import type { User, BookMark } from '@prisma/client';
+
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   signin() {
     return { message: 'I am signin' };
   }
 
-  signup() {
+  signup(dto: AuthDto) {
+    
     return { message: 'I am signup' };
   }
 }
