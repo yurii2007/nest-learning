51f51cd1503557d42f0f0a300e48ec7d5fcc572f
yurii2007 <yurash862@gmail.com>
2023-12-09 19:26:00 +0200

    (config module)(signIn && signUp)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookMark } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookMark, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    BookMark,
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,47 @@
 import type { User, BookMark } from '@prisma/client';
+import type { AuthDto } from './dto';
 
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import * as argon from 'argon2';
+
 import { PrismaService } from 'src/prisma/prisma.service';
-import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signin() {
-    return { message: 'I am signin' };
+  async signup(dto: AuthDto) {
+    try {
+      const hash = await argon.hash(dto.password);
+
+      const user = await this.prisma.user.create({
+        data: { email: dto.email, hash },
+        select: { email: true, username: true },
+      });
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('User with this email already exist');
+        }
+      }
+    }
   }
 
-  signup(dto: AuthDto) {
-    
-    return { message: 'I am signup' };
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findFirst({
+      where: { email: dto.email },
+    });
+
+    if (!user) throw new ForbiddenException('Email or password is wrong');
+
+    const isPasswordMatch = await argon.verify(user.hash, dto.password);
+
+    if (!isPasswordMatch)
+      throw new ForbiddenException('Email or password is wrong');
+
+    delete user.hash;
+
+    return user;
   }
 }
